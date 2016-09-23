9261644ce5c1fb6747374fe49b222c0521f48c32
marcusmolchany <marcusmolchany@gmail.com>
2016-09-23 10:45:11 -0700

    Add prices to Item Component

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,11 +5,15 @@ export default class Item extends React.Component {
     return (this.props.item.price / this.props.salary) * 100
   }
 
+  _getPrettyPercentage (percentage) {
+    return percentage === Infinity ? '-' : percentage.toFixed(2)
+  }
+
   render () {
     return (
       <div>
-        <h3>{this.props.item.name}</h3>
-        <span>{this._getPercentageOfSalary()}% of your salary</span>
+        <h3>{this.props.item.name} - (${this.props.item.price})</h3>
+        <span>{this._getPrettyPercentage(this._getPercentageOfSalary())}% of your salary</span>
       </div>
     )
   }
