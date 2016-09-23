f4607db28134c133181701ec5a6d37257f2a5d12
marcusmolchany <marcusmolchany@gmail.com>
2016-09-23 11:32:03 -0700

    Add new items to initial reducer state

--- src/core/items/reducer.js
+++ src/core/items/reducer.js
@@ -1,7 +1,11 @@
 const initialState = [
   {name: 'coffee', price: 3.50},
   {name: 'poke', price: 12.00},
-  {name: 'beer', price: 7.00}
+  {name: 'beer', price: 7.00},
+  {name: 'coachella', price: 350.00},
+  {name: 'iphone', price: 600.00},
+  {name: 'spotify', price: 12.00},
+  {name: 'yeezys', price: 250.00}
 ]
 
 export default function (state = initialState, action) {
