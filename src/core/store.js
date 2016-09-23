e7c191ef27b88ea0a8437135308afbd985c4b204
marcusmolchany <marcusmolchany@gmail.com>
2016-09-23 12:01:26 -0700

    Add redux devToolsExtension to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,11 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
-
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(thunkMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ))
 
   return store;
 }
