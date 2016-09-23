40dc616f53b5787f458bed39b130464fa5a71942
marcusmolchany <marcusmolchany@gmail.com>
2016-09-23 15:23:02 -0700

    Update _getPercentageOfSalary in Item component

--- src/views/components/Item.js
+++ src/views/components/Item.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default class Item extends React.Component {
-  _getPercentageOfSalary () {
-    return (this.props.item.price / this.props.salary) * 100
+  _getPercentageOfSalary (price) {
+    return (price / this.props.salary) * 100
   }
 
   _getPrettyPercentage (percentage) {
@@ -13,7 +13,7 @@ export default class Item extends React.Component {
     return (
       <div>
         <h3>{this.props.item.name} - (${this.props.item.price})</h3>
-        <span>{this._getPrettyPercentage(this._getPercentageOfSalary())}% of your salary</span>
+        <span>{this._getPrettyPercentage(this._getPercentageOfSalary(this.props.item.price))}% of your salary</span>
       </div>
     )
   }
