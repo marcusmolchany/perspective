103ea14b8fd95ca7a8f01662b77e77dc711b484e
marcusmolchany <marcusmolchany@gmail.com>
2016-09-23 16:10:44 -0700

    Show values in terms of post tax salary

--- src/views/containers/ItemsList.js
+++ src/views/containers/ItemsList.js
@@ -4,12 +4,13 @@ import { connect } from 'react-redux'
 import Item from '../components/Item'
 import { itemsSelectors } from '../../core/items'
 import { salarySelectors } from '../../core/salary'
+import { taxSelectors } from '../../core/tax'
 
 class ItemsList extends React.Component {
   render () {
     return (
       <div>
-        {this.props.items.map((item, id) => <Item key={id} item={item} salary={this.props.salary} />)}
+        {this.props.items.map((item, id) => <Item key={id} item={item} salary={this.props.salary * (1 - this.props.tax)} />)}
       </div>
     )
   }
@@ -18,7 +19,8 @@ class ItemsList extends React.Component {
 const mapStateToProps = (state) => {
   return {
     items: itemsSelectors.getItems(state),
-    salary: salarySelectors.getSalary(state)
+    salary: salarySelectors.getSalary(state),
+    tax: taxSelectors.getTax(state)
   }
 }
 const mapDispatchToProps = null
--- src/views/containers/SalaryPeriodBreakdown.js
+++ src/views/containers/SalaryPeriodBreakdown.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { salarySelectors } from '../../core/salary'
+import { taxSelectors } from '../../core/tax'
 
 /**
  * period enum types
@@ -16,25 +17,26 @@ class SalaryPeriodBreakdown extends React.Component {
   /**
    * calculate the salary per time period
    * @param  {Number} salary the user's salary
+   * @param  {Number} tax    the user's tax
    * @param  {Number} period time period
    * @return {String}        localeString of the salary per period
    */
-  _getPeriodSalary (salary, period) {
-    return (salary / period).toLocaleString()
+  _getPeriodSalary (salary, tax, period) {
+    return ((salary * (1 - tax)) / period).toLocaleString()
   }
 
   render () {
     return (
       <div>
-        <h3>Salary Breakdown</h3>
+        <h3>Salary Breakdown (Post Tax)</h3>
         <table>
           <tbody>
             <tr>
-              <td>Annual    - ${this._getPeriodSalary(this.props.salary, ANNUAL)}</td><td> &middot; </td>
-              <td>Monthly   - ${this._getPeriodSalary(this.props.salary, MONTHLY)}</td><td> &middot; </td>
-              <td>Bi-weekly - ${this._getPeriodSalary(this.props.salary, BIWEEKLY)}</td><td> &middot; </td>
-              <td>Weekly    - ${this._getPeriodSalary(this.props.salary, WEEKLY)}</td><td> &middot; </td>
-              <td>Daily     - ${this._getPeriodSalary(this.props.salary, DAILY)}</td>
+              <td>Annual    - ${this._getPeriodSalary(this.props.salary, this.props.tax, ANNUAL)}</td><td> &middot; </td>
+              <td>Monthly   - ${this._getPeriodSalary(this.props.salary, this.props.tax, MONTHLY)}</td><td> &middot; </td>
+              <td>Bi-weekly - ${this._getPeriodSalary(this.props.salary, this.props.tax, BIWEEKLY)}</td><td> &middot; </td>
+              <td>Weekly    - ${this._getPeriodSalary(this.props.salary, this.props.tax, WEEKLY)}</td><td> &middot; </td>
+              <td>Daily     - ${this._getPeriodSalary(this.props.salary, this.props.tax, DAILY)}</td>
             </tr>
           </tbody>
         </table>
@@ -45,7 +47,8 @@ class SalaryPeriodBreakdown extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    salary: salarySelectors.getSalary(state)
+    salary: salarySelectors.getSalary(state),
+    tax: taxSelectors.getTax(state)
   }
 }
 
